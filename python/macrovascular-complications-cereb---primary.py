9f0c94aa8c0945924aa08f036123ca03a3e9f5f5
Martin Chapman <contact@martinchapman.co.uk>
2024-05-24 20:04:18 +0100

    Update made by Phenoflow

---
+++ python/macrovascular-complications-cereb---primary.py
@@ -0,0 +1,25 @@
+# Evangelos Kontopantelis, David A Springate, David Reeves, Darren M. Aschroff, Martin Rutter, Iain Buchan, Tim Doran, Matthias Pierce, Darren M. Ashcroft, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"G640.00","system":"readv2"},{"code":"G64z200","system":"readv2"},{"code":"G662.00","system":"readv2"},{"code":"G61..12","system":"readv2"},{"code":"G64..12","system":"readv2"},{"code":"G61..11","system":"readv2"},{"code":"G641.11","system":"readv2"},{"code":"Gyu6400","system":"readv2"},{"code":"G640000","system":"readv2"},{"code":"Gyu6200","system":"readv2"},{"code":"G618.00","system":"readv2"},{"code":"G641000","system":"readv2"},{"code":"G61z.00","system":"readv2"},{"code":"G664.00","system":"readv2"},{"code":"G613.00","system":"readv2"},{"code":"G61..00","system":"readv2"},{"code":"G64z.12","system":"readv2"},{"code":"G617.00","system":"readv2"},{"code":"G64z.00","system":"readv2"},{"code":"G661.00","system":"readv2"},{"code":"G64z300","system":"readv2"},{"code":"G641.00","system":"readv2"},{"code":"G671000","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('macrovascular-complications-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["macrovascular-complications-cereb---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["macrovascular-complications-cereb---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["macrovascular-complications-cereb---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
